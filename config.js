87042ef47d29a0cac14461831f641b08a45565a8
kynsts-commits <kyn.sts@icloud.com>
2026-09-15 20:50:57 +0900

    Add Supabase configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+// Training Manager / Supabase connected configuration
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://gdfhiikjiajudqxujozp.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_UvSdAbrcJiK_kLVP-Cu7cA_NXtIx_tV",
+  VAPID_PUBLIC_KEY: "BDISAwFTk--VIjyJ-vwtmv1f3yU8mHJbhq2wmWsR-aYrAuFxVmvwG7YYC7RbBc9atpeCG6fR6QZc3pl6hELKE0A"
+};
