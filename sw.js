4c28a402560624a0964e4b4431e2f4c321fa2ae5
kynsts-commits <kyn.sts@icloud.com>
2026-09-16 21:49:07 +0900

    Remove body photo import cache change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='training-manager-v7';
+const CACHE='training-manager-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./nutrition-web.js','./health-display.js','./config.js','./manifest.webmanifest','./chatgpt-nutrition.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
